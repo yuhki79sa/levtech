516134e94642207b6db700e3c48f71a20611f09c
yuhki <yuhki79sa@gmail.com>
2024-06-12 16:11:12 +0900

    ブログ投稿一覧の表示

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+  //  return view('posts/index');
+// });
 
-use App\Http\Controllers\PostController; 
+//use App\Http\Controllers\PostController; 
 
-Route::get('/posts', [PostController::class, 'index']);
+//Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/', [PostController::class, 'index']);
